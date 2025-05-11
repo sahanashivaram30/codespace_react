6b2abed600b9b2e5fc7c6bc7887208b013647c88
sahanashivaram30 <sahanashivaram30@gmail.com>
2025-05-11 07:41:15 +0000

    Fixed import issue for Ques_10_Local_Storage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Ques_9_Dependent_API_Call from "./Module 3/React Hooks - useState & useEffect/Ques_9_Dependent_API_Call";
-
+import Local_Storage from "./Module 3/React Hooks - useState & useEffect/Ques_10_Local_Storage";  // Ensure correct file path
 
 function App() {
   return (
     <div>
-      <h1>React Dependent API Call Example</h1>
-      <Ques_9_Dependent_API_Call />
+      <h1>React Local Storage Example</h1>
+      <Local_Storage />
     </div>
   );
 }
