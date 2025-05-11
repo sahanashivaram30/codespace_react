18cde2de5415ebd7d4b8e626a387be0af2fea10c
sahanashivaram30 <sahanashivaram30@gmail.com>
2025-05-11 05:16:38 +0000

    Completed Ques_3: Greeting component added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './Module 3/Introduction to ReactJS/Ques_3';
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
--- src/Module 3/Introduction to ReactJS/Ques_2.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_2.jsx
@@ -1,11 +1,6 @@
-// Embedding JavaScript Expressions in JSX
-// Description: Embed a JavaScript expression in JSX to display the current year.
-// Use this component in App.js
 function Ques_2() {
   const year = new Date().getFullYear();
   return <div>The current year is {year}</div>;
 }
 
 export default Ques_2;
-
-
